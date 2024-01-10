10779f27fa3f21d8c2918233ae825614cba3af98
farhanzky <zakinurrafi@gmail.com>
2024-01-10 17:40:39 +0700

    g

---
+++ router/jobsheet.js
@@ -0,0 +1,8 @@
+const { JSONB } = require('sequelize');
+const jobsheetController = require('../controllers/jobsheet');
+const router = require('express').Router();
+
+router.post('/one', jobsheetController.submitOne);
+router.post('/many', jobsheetController.submitMany);
+
+module.exports = router;
